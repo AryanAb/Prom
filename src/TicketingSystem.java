693c8abf89d1981c07664587ec300a30821d2136
nischaylando3 <nischaylando4@gmail.com>
2020-02-19 20:48:39 -0500

    The Submission checker Works Now

--- src/TicketingSystem.java
+++ src/TicketingSystem.java
@@ -1,12 +1,9 @@
-//TODO Make sure every field is filled before submitting
 
-import org.w3c.dom.Text;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.ParseException;
 import java.util.ArrayList;
 
 public class TicketingSystem extends JPanel {
